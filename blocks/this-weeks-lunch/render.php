fbe66ff62cc7fb34fc73a570dabdc8eff1310082
Niku Hietanen <niku@shipit.fi>
2023-01-10 11:47:40 +0200

    Remove unnecessary code

--- blocks/this-weeks-lunch/render.php
+++ blocks/this-weeks-lunch/render.php
@@ -1,17 +1,7 @@
 <?php
-$days = [
-    'monday' => 'Maanantai',
-    'tuesday' => 'Tiistai',
-    'wednesday' => 'Keskiviikko',
-    'thursday' => 'Torstai',
-    'friday' => 'Perjantai',
-    'saturday' => 'Lauantai',
-    'sunday' => 'Sunnuntai',
-];
-
 $lunchList = \LunchList\LunchList::getThisWeeksLunchList();
-
 ?>
+
 <h2>Viikon <?php echo esc_html($lunchList->title); ?> lounaslista</h2>
 
 <?php foreach ($lunchList->days as $day => $content) : ?>
